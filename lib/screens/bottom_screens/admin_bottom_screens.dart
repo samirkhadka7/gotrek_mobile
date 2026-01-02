b1c34639f8e69c4ede0ebd8f36d0ad1bef6a9c02
Samir <khadkasamir8079@gmail.com>
2026-01-02 16:54:59 +0545

    Resolve pubspec.lock merge conflicts

---
+++ lib/screens/bottom_screens/admin_bottom_screens.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AdminBottomScreens extends StatelessWidget {
+  const AdminBottomScreens({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox.expand(
+      child: Center(
+        child: Text("Admins"),
+      ),
+    );
+  }
+}
---
+++ lib/screens/bottom_screens/profile_bottom_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ProfileBottomScreen extends StatelessWidget {
+  const ProfileBottomScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox.expand(
+      child: Center(
+        child: Text("Profiles"),
+      ),
+    );
+  }
+}
