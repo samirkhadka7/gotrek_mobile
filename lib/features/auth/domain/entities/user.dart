2869d263d3cc6f51ea6c7e7d9a9e055707d8de5f
Samir <khadkasamir8079@gmail.com>
2026-01-16 11:07:49 +0545

    entity

---
+++ lib/features/auth/domain/entities/user.dart
@@ -0,0 +1,31 @@
+class User {
+  final String id;
+  final String name;
+  final String email;
+  final String password;
+
+  User({
+    required this.id,
+    required this.name,
+    required this.email,
+    required this.password,
+  });
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+      id: json['id'] as String,
+      name: json['name'] as String,
+      email: json['email'] as String,
+      password: json['password'] as String,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'email': email,
+      'password': password,
+    };
+  }
+}
