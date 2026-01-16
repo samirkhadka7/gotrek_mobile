2038d6b5a0e271269da2d8f95f361b35950efb4b
Samir <khadkasamir8079@gmail.com>
2026-01-16 11:05:32 +0545

    Add data models

---
+++ lib/features/auth/data/models/user_hive_model.dart
@@ -0,0 +1,43 @@
+import 'package:hive/hive.dart';
+
+part 'user_hive_model.g.dart';
+
+@HiveType(typeId: 0)
+class UserHiveModel extends HiveObject {
+  @HiveField(0)
+  final String id;
+
+  @HiveField(1)
+  final String name;
+
+  @HiveField(2)
+  final String email;
+
+  @HiveField(3)
+  final String password;
+
+  UserHiveModel({
+    required this.id,
+    required this.name,
+    required this.email,
+    required this.password,
+  });
+
+  factory UserHiveModel.fromJson(Map<String, dynamic> json) {
+    return UserHiveModel(
+      id: json['id'] as String,
+      name: json['name'] as String,
+      email: json['email'] as String,
+      password: json['password'] as String,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'email': email,
+      'password': password,
+    };
+  }
+}
