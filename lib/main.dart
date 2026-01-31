a3b8ac95a4be95b2117b46f46cfc7cc1734b935f
Samir <khadkasamir8079@gmail.com>
2026-01-31 12:11:27 +0545

    injection_container

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,31 @@
 import 'package:flutter/material.dart';
-import 'package:hive_flutter/hive_flutter.dart';
-import 'package:provider/provider.dart';
-import 'package:gotrek/core/constants/hive_constants.dart';
-import 'package:gotrek/core/services/hive_service.dart';
-import 'package:gotrek/features/auth/data/datasources/auth_local_data_source.dart';
-import 'package:gotrek/features/auth/data/repositories/auth_repository_impl.dart';
-import 'package:gotrek/features/auth/domain/usecases/register_user.dart';
-import 'package:gotrek/features/auth/domain/usecases/login_user.dart';
-import 'package:gotrek/features/auth/domain/usecases/is_user_registered.dart';
-import 'package:gotrek/features/auth/presentation/providers/auth_provider.dart';
-import 'package:gotrek/screens/splash_screen.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'core/network/hive_service.dart';
+import 'app/theme/app_theme.dart';
+import 'features/auth/presentation/bloc/auth_bloc.dart';
+import 'features/auth/presentation/bloc/auth_event.dart';
+import 'features/splash/presentation/pages/splash_screen.dart';
+import 'injection_container.dart' as di;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  
   await HiveService.init();
-  runApp(MyApp());
+  await di.initDependencies();
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        Provider<AuthLocalDataSource>(
-          create: (_) => AuthLocalDataSourceImpl(Hive.box(HiveConstants.userBox)),
-        ),
-        Provider<AuthRepositoryImpl>(
-          create: (context) => AuthRepositoryImpl(context.read<AuthLocalDataSource>()),
-        ),
-        Provider<RegisterUser>(
-          create: (context) => RegisterUser(context.read<AuthRepositoryImpl>()),
-        ),
-        Provider<LoginUser>(
-          create: (context) => LoginUser(context.read<AuthRepositoryImpl>()),
-        ),
-        Provider<IsUserRegistered>(
-          create: (context) => IsUserRegistered(context.read<AuthRepositoryImpl>()),
-        ),
-        ChangeNotifierProvider<AuthProvider>(
-          create: (context) => AuthProvider(
-            registerUser: context.read<RegisterUser>(),
-            loginUser: context.read<LoginUser>(),
-            isUserRegistered: context.read<IsUserRegistered>(),
-          ),
-        ),
-      ],
+    return BlocProvider(
+      create: (_) => di.sl<AuthBloc>()..add(CheckAuthStatusEvent()),
       child: MaterialApp(
+        title: 'GOTrek',
         debugShowCheckedModeBanner: false,
-        title: 'Trek Nepal',
+        theme: AppTheme.getApplicationTheme(),
         home: SplashScreen(),
       ),
     );
