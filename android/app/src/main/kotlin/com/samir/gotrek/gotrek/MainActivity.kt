71f06336f8c0be83cff5905d0011d022144e4a3b
Samir <khadkasamir8079@gmail.com>
2026-03-05 01:22:42 +0545

    Add Android native integrations

--- android/app/src/main/kotlin/com/samir/gotrek/gotrek/MainActivity.kt
+++ android/app/src/main/kotlin/com/samir/gotrek/gotrek/MainActivity.kt
@@ -1,5 +1,21 @@
 package com.samir.gotrek.gotrek
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.EventChannel
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterFragmentActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+
+        EventChannel(
+            flutterEngine.dartExecutor.binaryMessenger,
+            LightSensorChannel.CHANNEL,
+        ).setStreamHandler(LightSensorChannel(applicationContext))
+
+        EventChannel(
+            flutterEngine.dartExecutor.binaryMessenger,
+            BarometerChannel.CHANNEL,
+        ).setStreamHandler(BarometerChannel(applicationContext))
+    }
+}
